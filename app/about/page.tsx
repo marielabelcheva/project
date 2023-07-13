0c9f86c4b7ea97b767dacbdf1f9fbdadcb023195
marielabelcheva <meribelcheva@gmail.com>
2023-07-13 15:32:42 +0300

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,23 @@
+export default function About() {
+  return (
+  <div>
+    <div className="container">
+        <h2 className="section-title">Gallery</h2>
+        <div className="gallery">
+          <img className="gallery-image" src="/cake 1.png" alt="cake 1" />
+          <img className="gallery-image" src="/cake 2.png" alt="cake 2" />
+          <img className="gallery-image" src="/cake 3.png" alt="cake 3" />
+          <img className="gallery-image" src="/cake 3.2.png" alt="cake 3.2" />
+          <img className="gallery-image" src="/cake 4.png" alt="cake 4" />
+          <img className="gallery-image" src="/cake 5.png" alt="cake 5" />
+          <img className="gallery-image" src="/cake 6.png" alt="cake 6" />
+          <img className="gallery-image" src="/cake 7.png" alt="cake 7" />
+          <img className="gallery-image" src="/cake 8.png" alt="cake 8" />
+          <img className="gallery-image" src="/cake 9.png" alt="cake 9" />
+          <img className="gallery-image" src="/cake 10.png" alt="cake 10" />
+          <img className="gallery-image" src="/cake 11.png" alt="cake 11" />
+        </div>
+      </div>
+  </div>
+  );
+}
